87369178d96db9c3683c0764d2a665ef569c514b
Darshan Hirekurbar <darshanhirekurbar@Darshans-MacBook-Air.local>
2025-11-20 23:19:32 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup # type: ignore
+
+setup(
+    name="medical_chatbot",
+    version="0.0.0",
+    author="Harshit",
+    author_email="harshithirekurubar@gmail.com",
+    packages= find_packages(),
+    install_requires = []
+)
